34ee52167254db1359705dc9ef2a01090b6c57ef
ambition-hb <157391873@qq.com>
2019-10-19 17:06:55 +0800

    Submit CardViewDemo

---
+++ app/src/main/java/com/haobi/cardviewdemo/MainActivity.java
@@ -0,0 +1,82 @@
+package com.haobi.cardviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CardView mCardView;
+    private SeekBar sb1;
+    private SeekBar sb2;
+    private SeekBar sb3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        assignViews();
+    }
+
+    public void assignViews(){
+        mCardView = (CardView)findViewById(R.id.tv_item);
+        sb1 = (SeekBar)findViewById(R.id.sb_1);
+        sb2 = (SeekBar)findViewById(R.id.sb_2);
+        sb3 = (SeekBar)findViewById(R.id.sb_3);
+        sb1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // 设置圆角半径
+                mCardView.setRadius(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        sb2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // 设置阴影半径
+                mCardView.setCardElevation(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        sb3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // 设置子控件与父控件的距离
+                mCardView.setContentPadding(progress, progress, progress, progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
